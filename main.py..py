0ae5fd5de1983fde8c2f32c9a618e32fff0cb696
Alissia-a11y <alisatklnk@gmail.com>
2024-12-13 01:46:02 +0500

    Add files via upload

---
+++ main.py..py
@@ -0,0 +1,12 @@
+print((9**0.5)*5)    #1st program
+
+print(0.99>0.98 and 1000!= 1000.1)    #2nd program
+
+print(2*2+2)    #3rd program
+print((2+2)*2)
+print((2*2+2)==(2+2)*2)
+
+print(type(float('123.456')))    #4th program
+print(123.456*10)
+print(1234//1)
+print(1234%10)
