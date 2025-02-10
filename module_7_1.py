431b4bb437460a1627ac5fb14964bbe9308c89e2
Alissia-a11y <alisatklnk@gmail.com>
2025-02-10 17:57:29 +0500

    Update module_7_1.py

--- module_7_1.py
+++ module_7_1.py
@@ -1,8 +1,8 @@
 class Product:
     def __init__(self, name: str, weight: float, category: str):
-        self.name = name  # название продукта
-        self.weight = weight  # общий вес товара
-        self.category = category  # категория товара
+        self.name = name 
+        self.weight = weight 
+        self.category = category
 
     def __str__(self):
         return f'{self.name}, {self.weight}, {self.category}'
@@ -18,14 +18,12 @@ class Shop:
         file.close()
         return content
 
-    # Задание не соответствует примеру работы программы.
-    # Задание: добавить продукт в файл, если его названия нет в файле.
     def add(self, *products: Product):
-        names = []  # список названий продуктов, содержащихся в файле
+        names = [] 
         file = open(self.__file_name, 'a+')
-        file.seek(0)  # режим 'а' устанавливает курсор на конец файла
+        file.seek(0) 
         for line in file:
-            name = line.split(', ')[0]  # название продукта
+            name = line.split(', ')[0]
             names.append(name)
         for product in products:
             if product.name in names:
@@ -35,20 +33,6 @@ class Shop:
                 names.append(product.name)
         file.close()
 
-    # Добавление продукта согласно примеру работы программы
-    # def add(self, *products: Product):
-    #     file_products = []  # список продуктов, содержащихся в файле
-    #     file = open(self.__file_name, 'a+')
-    #     file.seek(0)
-    #     for line in file:
-    #         file_products.append(line.rstrip())  # без '\n'
-    #     for product in products:
-    #         if str(product) in file_products:
-    #             print(f'Продукт {product} уже есть в магазине')
-    #         else:
-    #             file.write(f'{product}\n')
-    #     file.close()
-
 
 if __name__ == '__main__':
     s1 = Shop()
@@ -56,8 +40,7 @@ if __name__ == '__main__':
     p2 = Product('Spaghetti', 3.4, 'Groceries')
     p3 = Product('Potato', 5.5, 'Vegetables')
 
-    print(p2)  # __str__
-
+    print(p2)  
     s1.add(p1, p2, p3)
 
     print(s1.get_products())
